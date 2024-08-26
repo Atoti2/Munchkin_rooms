e9ed1034528fe4ff4b9ec58aa5ad77363dbc4502
Atoti2 <bogyikormi3@gmail.com>
2024-08-26 17:29:52 +0200

    Final

--- server/index.js
+++ server/index.js
@@ -87,10 +87,10 @@ io.on('connection', (socket) => {
   });
 });
 
-app.use(express.static(path.join(__dirname, 'client/dist')));
+app.use(express.static(path.join(__dirname, '../client/munchkin/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/munchkin/dist', 'index.html'));
 });
 
 server.listen(3001, () => {
